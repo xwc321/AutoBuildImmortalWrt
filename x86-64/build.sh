391e201ec0c0986326f8d55ae4b1baf074062661
xwc321 <167732256+xwc321@users.noreply.github.com>
2025-07-08 07:43:32 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -37,7 +37,6 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-filebrowser-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
