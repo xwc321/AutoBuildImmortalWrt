d261d4e5dcf8a333978d1db9cc4625d6abb866f4
xwc321 <167732256+xwc321@users.noreply.github.com>
2025-11-14 20:10:43 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -64,6 +64,7 @@ PACKAGES="$PACKAGES luci-i18n-uhttpd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
 PACKAGES="$PACKAGES luci-app-unblockneteasemusic"
 # PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-bandix-zh-cn
 
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
