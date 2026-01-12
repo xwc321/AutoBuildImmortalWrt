d3dbd063802cc94f9d869fe6145b84fcd9e37e71
xwc321 <167732256+xwc321@users.noreply.github.com>
2026-01-12 16:15:39 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -65,8 +65,6 @@ PACKAGES="$PACKAGES luci-i18n-usb-printer-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ddns-go-zh-cn"
 PACKAGES="$PACKAGES luci-app-unblockneteasemusic"
 PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-usb-printer-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
